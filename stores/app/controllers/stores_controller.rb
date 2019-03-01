9f5264c8f4d132f63e8f5536823e8a36f506ec0f
YudiR <yudrose13@gmail.com>
2019-03-01 13:43:09 -0500

    setup routes, index, model and seeds.

---
+++ stores/app/controllers/stores_controller.rb
@@ -0,0 +1,23 @@
+class StoresController < ApplicationController
+  def index
+    @stores = Store.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ stores/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'stores#index'
+
+  resources   :stores
+
+end
