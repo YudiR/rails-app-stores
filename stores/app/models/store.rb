076c4aecc42caa9c1470ef1f5651f8bb671a190f
YudiR <yudrose13@gmail.com>
2019-04-14 11:42:09 -0400

    set up app for production. changed db for production from sqlite3 to postgresql

--- stores/app/models/store.rb
+++ stores/app/models/store.rb
@@ -1,2 +1,5 @@
 class Store < ApplicationRecord
+    # validates :location, confirmation: true
+    # validates :location_confirmation, presence: true
+    validates :name, length: { minimum: 3, too_short: "%{count} letters is least needed for the name" }
 end
